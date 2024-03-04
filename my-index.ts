b2facb3251fd272550458ce741a75eb04821cefb
Havn <post@havn.online>
2024-03-04 13:04:13 +0100

    improved double modifiers

--- my-index.ts
+++ my-index.ts
@@ -30,6 +30,7 @@ import {
   toKey,
   toStickyModifier,
   BasicParameters,
+  toNone,
 } from "karabiner.ts";
 
 writeToProfile(
@@ -38,10 +39,12 @@ writeToProfile(
     rule("Caps + Quote -> Hyper").manipulators([
       map("⇪")
         .toIfAlone("⇪", {}, { halt: true })
-        .toDelayedAction(
-          toKey("vk_none"),
+        .toDelayedAction(toNone(), [
           toStickyModifier("left_shift", "toggle"),
-        )
+          toStickyModifier("left_control", "toggle"),
+          toStickyModifier("left_option", "toggle"),
+          toStickyModifier("left_command", "toggle"),
+        ])
         .toIfHeldDown("l⇧", "l⌘⌥⌃", { halt: true }),
       map("'")
         .toIfAlone("'", {}, { halt: true })
@@ -49,20 +52,61 @@ writeToProfile(
         .toIfHeldDown("r⇧", "r⌘⌥⌃", { halt: true })
         .parameters({ "basic.to_if_held_down_threshold_milliseconds": 220 }),
     ]),
-    // Four - left hand
+
+    // Home row mods
     rule("Home row mods - shift, ctrl, opt, cmd").manipulators([
+      //
+      // Four - left hand
       mapSimultaneous(["a", "s", "d", "f"]).toIfHeldDown("l⇧", ["l⌘⌥⌃"]),
+      //
       // Three - left hand
       mapSimultaneous(["a", "s", "d"]).toIfHeldDown("l⇧", ["l⌥⌃"]),
       mapSimultaneous(["a", "d", "f"]).toIfHeldDown("l⇧", ["l⌘⌥"]),
       mapSimultaneous(["s", "d", "f"]).toIfHeldDown("l⌃", ["l⌘⌥"]),
+      //
       // Two - left hand
-      mapSimultaneous(["a", "s"]).toIfHeldDown("l⇧", "l⌃"),
-      mapSimultaneous(["a", "d"]).toIfHeldDown("l⇧", "l⌥"),
+      mapSimultaneous(["a", "s"], { key_down_order: "strict" })
+        .toIfAlone("a")
+        .toIfAlone("s")
+        .toIfHeldDown("l⇧", "l⌃"),
+      mapSimultaneous(["s", "a"], { key_down_order: "strict" })
+        .toIfAlone("s")
+        .toIfAlone("a")
+        .toIfHeldDown("l⇧", "l⌃"),
+      mapSimultaneous(["a", "d"], { key_down_order: "strict" })
+        .toIfAlone("a")
+        .toIfAlone("d")
+        .toIfHeldDown("l⇧", "l⌥"),
+      mapSimultaneous(["d", "a"], { key_down_order: "strict" })
+        .toIfAlone("d")
+        .toIfAlone("a")
+        .toIfHeldDown("l⇧", "l⌥"),
       mapSimultaneous(["a", "f"]).toIfHeldDown("l⇧", "l⌘"),
-      mapSimultaneous(["s", "d"]).toIfHeldDown("l⌃", "l⌥"),
-      mapSimultaneous(["s", "f"]).toIfHeldDown("l⌃", "l⌘"),
-      mapSimultaneous(["d", "f"]).toIfHeldDown("l⌥", "l⌘"),
+      mapSimultaneous(["s", "d"], { key_down_order: "strict" })
+        .toIfAlone("s")
+        .toIfAlone("d")
+        .toIfHeldDown("l⌃", "l⌥"),
+      mapSimultaneous(["d", "s"], { key_down_order: "strict" })
+        .toIfAlone("d")
+        .toIfAlone("s")
+        .toIfHeldDown("l⌃", "l⌥"),
+      mapSimultaneous(["s", "f"], { key_down_order: "strict" })
+        .toIfAlone("s")
+        .toIfAlone("f")
+        .toIfHeldDown("l⌃", "l⌘"),
+      mapSimultaneous(["f", "s"], { key_down_order: "strict" })
+        .toIfAlone("f")
+        .toIfAlone("s")
+        .toIfHeldDown("l⌃", "l⌘"),
+      mapSimultaneous(["d", "f"], { key_down_order: "strict" })
+        .toIfAlone("d")
+        .toIfAlone("f")
+        .toIfHeldDown("l⌥", "l⌘"),
+      mapSimultaneous(["f", "d"], { key_down_order: "strict" })
+        .toIfAlone("f")
+        .toIfAlone("d")
+        .toIfHeldDown("l⌥", "l⌘"),
+      //
       // One - left hand
       map("a")
         .toIfAlone("a", {}, { halt: true })
@@ -81,19 +125,64 @@ writeToProfile(
         .toDelayedAction(toKey("vk_none"), toKey("f", {}, { halt: true }))
         .toIfHeldDown("l⌘", {}, { halt: true }),
       //
+      //
       // Four - right hand
       mapSimultaneous(["j", "k", "l", ";"]).toIfHeldDown("r⇧", ["r⌘⌥⌃"]),
+      //
       // Three - right hand
       mapSimultaneous([";", "l", "k"]).toIfHeldDown("r⇧", ["r⌥⌃"]),
       mapSimultaneous([";", "k", "j"]).toIfHeldDown("r⇧", ["r⌘⌥"]),
       mapSimultaneous(["l", "k", "j"]).toIfHeldDown("r⌃", ["r⌘⌥"]),
+      //
       // Two - right hand
-      mapSimultaneous([";", "l"]).toIfHeldDown("r⇧", "r⌃"),
-      mapSimultaneous([";", "k"]).toIfHeldDown("r⇧", "r⌥"),
-      mapSimultaneous([";", "j"]).toIfHeldDown("r⇧", "r⌘"),
-      mapSimultaneous(["l", "k"]).toIfHeldDown("r⌃", "r⌥"),
-      mapSimultaneous(["l", "j"]).toIfHeldDown("r⌃", "r⌘"),
-      mapSimultaneous(["k", "j"]).toIfHeldDown("r⌥", "r⌘"),
+      mapSimultaneous([";", "l"], { key_down_order: "strict" })
+        .toIfAlone(";")
+        .toIfAlone("l")
+        .toIfHeldDown("r⇧", "r⌃"),
+      mapSimultaneous(["l", ";"], { key_down_order: "strict" })
+        .toIfAlone("l")
+        .toIfAlone(";")
+        .toIfHeldDown("r⇧", "r⌃"),
+      mapSimultaneous([";", "k"], { key_down_order: "strict" })
+        .toIfAlone(";")
+        .toIfAlone("k")
+        .toIfHeldDown("r⇧", "r⌥"),
+      mapSimultaneous(["k", ";"], { key_down_order: "strict" })
+        .toIfAlone("k")
+        .toIfAlone(";")
+        .toIfHeldDown("r⇧", "r⌥"),
+      mapSimultaneous([";", "j"], { key_down_order: "strict" })
+        .toIfAlone(";")
+        .toIfAlone("j")
+        .toIfHeldDown("r⇧", "r⌘"),
+      mapSimultaneous(["j", ";"], { key_down_order: "strict" })
+        .toIfAlone("j")
+        .toIfAlone(";")
+        .toIfHeldDown("r⇧", "r⌘"),
+      mapSimultaneous(["l", "k"], { key_down_order: "strict" })
+        .toIfAlone("l")
+        .toIfAlone("k")
+        .toIfHeldDown("r⌃", "r⌥"),
+      mapSimultaneous(["k", "l"], { key_down_order: "strict" })
+        .toIfAlone("k")
+        .toIfAlone("l")
+        .toIfHeldDown("r⌃", "r⌥"),
+      mapSimultaneous(["l", "j"], { key_down_order: "strict" })
+        .toIfAlone("l")
+        .toIfAlone("j")
+        .toIfHeldDown("r⌃", "r⌘"),
+      mapSimultaneous(["j", "l"], { key_down_order: "strict" })
+        .toIfAlone("j")
+        .toIfAlone("l")
+        .toIfHeldDown("r⌃", "r⌘"),
+      mapSimultaneous(["k", "j"], { key_down_order: "strict" })
+        .toIfAlone("k")
+        .toIfAlone("j")
+        .toIfHeldDown("r⌥", "r⌘"),
+      mapSimultaneous(["j", "k"], { key_down_order: "strict" })
+        .toIfAlone("j")
+        .toIfAlone("k")
+        .toIfHeldDown("r⌥", "r⌘"),
       // One - right hand
       map("j")
         .toIfAlone("j", {}, { halt: true })
@@ -109,10 +198,7 @@ writeToProfile(
         .toIfHeldDown("r⌃", {}, { halt: true }),
       map(";")
         .toIfAlone(";", {}, { halt: true })
-        .toDelayedAction(
-          toKey("vk_none"),
-          toStickyModifier("right_shift", "toggle"),
-        )
+        .toDelayedAction(toKey("vk_none"), toKey(";"))
         .toIfHeldDown("r⇧", {}, { halt: true }),
     ]),
     // Meh
